0f5464d69bd5be5b10c8aa16bf6f1ffcf48e7fe2
hyusterr <chu1231104@gmail.com>
2024-05-09 02:20:31 +0800

    add py and nvim setups

---
+++ py-llm-packages.sh
@@ -0,0 +1,1 @@
+pip install transformers peft accelerate dataset evaluate wandb ray numpy pandas matplotlib huggingface_hub llama-cpp-python vllm sentencepiece trl BitsAndBytes deepspeed torch tqdm langchainhub
